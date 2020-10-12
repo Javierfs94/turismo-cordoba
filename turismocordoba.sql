cb5797f94a5b61aa93c2eb9c8d852b4f82da9dc5
javifs94 <javifs94@gmail.com>
2020-10-12 10:02:02 +0200

    Base de datos v3

--- turismocordoba.sql
+++ turismocordoba.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-10-2020 a las 19:12:59
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.1
+-- Tiempo de generación: 12-10-2020 a las 09:59:43
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,22 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `codigo`
+--
+
+CREATE TABLE `codigo` (
+  `id` int(11) NOT NULL,
+  `valor` int(11) NOT NULL,
+  `generador` int(11) NOT NULL,
+  `validado` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `empresa`
 --
 
 CREATE TABLE `empresa` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `gerente` int(11) NOT NULL,
-  `valoracion` int(1) NOT NULL
+  `id_gerente` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `empresa`
 --
 
-INSERT INTO `empresa` (`id`, `nombre`, `gerente`, `valoracion`) VALUES
-(1, 'McDonals', 0, 2);
+INSERT INTO `empresa` (`id`, `id_gerente`, `nombre`) VALUES
+(2, 6, 'Taberna El chiquitito');
 
 -- --------------------------------------------------------
 
@@ -72,37 +83,63 @@ CREATE TABLE `oferta` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `experiencia` int(11) NOT NULL,
+  `nivel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `perfil`
+--
+
+INSERT INTO `perfil` (`id`, `id_usuario`, `experiencia`, `nivel`) VALUES
+(1, 2, 0, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
-  `perfil` enum('admin','turista','gerente','') NOT NULL,
   `usuario` varchar(255) NOT NULL,
   `contraseña` varchar(255) NOT NULL,
-  `puntos` int(11) NOT NULL DEFAULT 0,
-  `nivel` int(11) NOT NULL DEFAULT 1
+  `rol` varchar(30) NOT NULL,
+  `auth_level` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `perfil`, `usuario`, `contraseña`, `puntos`, `nivel`) VALUES
-(0, 'admin', 'admin', 'admin123', 0, 1),
-(1, 'gerente', 'MacDonals', 'mcdonal123', 0, 1),
-(2, 'turista', 'pepeVilluela', 'usuario123', 0, 1);
+INSERT INTO `usuario` (`id`, `usuario`, `contraseña`, `rol`, `auth_level`) VALUES
+(2, 'pepeVilluela', 'usuario123', 'turista', 2),
+(5, 'admin', 'admin', 'admin', 9),
+(6, 'gerente', 'gerente', 'gerente', 8);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `codigo`
+--
+ALTER TABLE `codigo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `codigo_ibfk_1` (`generador`);
+
+--
 -- Indices de la tabla `empresa`
 --
 ALTER TABLE `empresa`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `gerente` (`gerente`);
+  ADD KEY `id_gerente` (`id_gerente`);
 
 --
 -- Indices de la tabla `establecimiento`
@@ -119,6 +156,13 @@ ALTER TABLE `oferta`
   ADD KEY `id_empresa` (`id_empresa`);
 
 --
+-- Indices de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
@@ -129,10 +173,16 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `codigo`
+--
+ALTER TABLE `codigo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `establecimiento`
@@ -147,20 +197,32 @@ ALTER TABLE `oferta`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `codigo`
+--
+ALTER TABLE `codigo`
+  ADD CONSTRAINT `codigo_ibfk_1` FOREIGN KEY (`generador`) REFERENCES `usuario` (`id`);
+
+--
 -- Filtros para la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  ADD CONSTRAINT `empresa_ibfk_1` FOREIGN KEY (`gerente`) REFERENCES `usuario` (`id`);
+  ADD CONSTRAINT `empresa_ibfk_1` FOREIGN KEY (`id_gerente`) REFERENCES `usuario` (`id`);
 
 --
 -- Filtros para la tabla `establecimiento`
@@ -173,6 +235,12 @@ ALTER TABLE `establecimiento`
 --
 ALTER TABLE `oferta`
   ADD CONSTRAINT `oferta_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id`);
+
+--
+-- Filtros para la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD CONSTRAINT `perfil_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
