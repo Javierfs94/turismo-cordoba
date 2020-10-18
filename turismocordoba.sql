8b9fb7504442b06c75787c0e73942d7800c871d5
RafaelUrbanoEstepa <55538311+RafaelUrbanoEstepa@users.noreply.github.com>
2020-10-18 13:52:50 +0200

    Add files via upload

--- turismocordoba.sql
+++ turismocordoba.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-10-2020 a las 20:50:00
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.2.34
+-- Tiempo de generación: 18-10-2020 a las 13:52:30
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -96,7 +97,27 @@ INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
 ('at8nr5plqjfan27nniiv6pg87ahttcsj', '127.0.0.1', 1602779554, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323737393535343b),
 ('4ipnj61udvugi3jjprmmhs99dpr60tm4', '127.0.0.1', 1602787223, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323738373232333b),
 ('3trgfd5sbhhci7qsnc6rrqgormi33jka', '127.0.0.1', 1602787747, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323738373734373b),
-('7jarucall7bl5k8sr78no9aiu09ba179', '127.0.0.1', 1602787781, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323738373734373b);
+('7jarucall7bl5k8sr78no9aiu09ba179', '127.0.0.1', 1602787781, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323738373734373b),
+('1ta9pplp159eog8vd4uqvfplmad82kmq', '::1', 1602927906, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323932373930363b),
+('fpn8ic2vvjrolm96pj06bsmibcfb76ff', '::1', 1602928254, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323932383235343b),
+('q28kilrqlfbnlmgn3erkd0494inv5oe8', '::1', 1602928647, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323932383634373b),
+('jpoktqpvi306891obkuepe3ja476c9s2', '::1', 1602929667, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323932393636373b),
+('makinaso2rn7vr4sbma4ouehtv6u8cid', '::1', 1602930298, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323933303239383b),
+('tfklluim1k12ni177mb2oijt2a3qfhen', '::1', 1602931061, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323933313036313b),
+('0kicjso9tch6h5m5gb4nfc3gov2bvus3', '::1', 1602931399, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323933313339393b),
+('jdfmaj60n31ohm66l877rpkv0k41r4rg', '::1', 1602933825, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323933333832353b),
+('8bgcjisd0cks9lnk23qhcu5kuj04v0rq', '::1', 1602935348, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323933353334383b),
+('3n4j0q69ufvd2bg0cef3fo5jn1dcn9se', '::1', 1602935850, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323933353835303b),
+('h704mos68b5rodo4t5fbqmmn8ncg2eni', '::1', 1602936178, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323933363137383b),
+('bb2b6f2djh4vpkesr40k4b9uf892r67a', '::1', 1602936453, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630323933363137383b),
+('jvu10uuo3ukof504dp2gm4sm7gjbmqpp', '::1', 1603011494, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630333031313439343b),
+('s8mv94jugfgb78oeg2qklqo5522dssb6', '::1', 1603011802, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630333031313830323b),
+('vb6520h3vp3v9dmd399tdleuqvf2v8pi', '::1', 1603012934, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630333031323933343b),
+('rthtebgd9gdkt6sopa7nnob4h165lce9', '::1', 1603013898, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630333031333839383b),
+('8e36fl8flcf1f8k2qakc7dkpnttqtgv6', '::1', 1603016990, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630333031363939303b),
+('lqfreeuq39q3lclk3ft6pp60601sb6u8', '::1', 1603017330, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630333031373333303b),
+('212tq9muom0p7efhii1utpi42n9bin55', '::1', 1603021379, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630333032313337393b),
+('n2j80c5bnl7kiaq7uvloiecsasmno7k2', '::1', 1603021379, 0x5f5f63695f6c6173745f726567656e65726174657c693a313630333032313337393b);
 
 -- --------------------------------------------------------
 
@@ -119,10 +140,18 @@ CREATE TABLE `denied_access` (
 
 CREATE TABLE `empresas` (
   `id` int(11) NOT NULL,
-  `id_gerente` int(11) NOT NULL,
+  `id_gerente` int(10) UNSIGNED NOT NULL,
   `nombre` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
   `valoracion` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `empresas`
+--
+
+INSERT INTO `empresas` (`id`, `id_gerente`, `nombre`, `valoracion`) VALUES
+(1, 1, 'Bar Paquita', 4),
+(2, 4, 'McDonals', 3);
 
 -- --------------------------------------------------------
 
@@ -133,10 +162,17 @@ CREATE TABLE `empresas` (
 CREATE TABLE `establecimientos` (
   `id` int(11) NOT NULL,
   `id_empresa` int(11) NOT NULL,
-  `id_dependiente` int(11) NOT NULL,
-  `nombre` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `nombre_establecimiento` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
   `direccion` varchar(120) COLLATE utf8mb4_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `establecimientos`
+--
+
+INSERT INTO `establecimientos` (`id`, `id_empresa`, `id_usuario`, `nombre_establecimiento`, `direccion`) VALUES
+(1, 2, 2, 'La Viona', 'https://goo.gl/maps/F6hzrMtCMMMpQRp18');
 
 -- --------------------------------------------------------
 
@@ -174,8 +210,30 @@ CREATE TABLE `ofertas` (
   `id_empresa` int(11) NOT NULL,
   `descripcion` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
   `tipo` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `codigos` varchar(6) COLLATE utf8mb4_spanish_ci NOT NULL
+  `codigos` varchar(6) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `puntos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `ofertas`
+--
+
+INSERT INTO `ofertas` (`id`, `id_empresa`, `descripcion`, `tipo`, `codigos`, `puntos`) VALUES
+(1, 1, 'Descuento en la consumición del 20%', 'Rebaja', '123ABC', 10),
+(2, 2, 'Sorteo de cascos', 'sorteo', '456FFF', 30);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `puntos` int(11) NOT NULL,
+  `nivel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -215,6 +273,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `auth_level`, `banned`, `passwd`, `passwd_recovery_code`, `passwd_recovery_date`, `passwd_modified_at`, `last_login`, `created_at`, `modified_at`) VALUES
+(2, 'FranciscoVio', 'fViona@gmail.com', 1, '0', 'asdasddqweqwdasda', NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', '2020-10-18 10:34:55'),
 (3402487300, 'admin', 'admin@turismocordoba.com', 9, '0', '$2y$11$jvyxgLP.nNeVyBKhNvuYGORbyiQw.4hN/o4xgOO0qfCUGbZVMghTS', NULL, NULL, NULL, NULL, '2020-10-15 18:20:34', '2020-10-15 16:20:34');
 
 --
@@ -279,13 +338,16 @@ ALTER TABLE `denied_access`
 -- Indices de la tabla `empresas`
 --
 ALTER TABLE `empresas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_gerente` (`id_gerente`);
 
 --
 -- Indices de la tabla `establecimientos`
 --
 ALTER TABLE `establecimientos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_empresa` (`id_empresa`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `ips_on_hold`
@@ -303,7 +365,15 @@ ALTER TABLE `login_errors`
 -- Indices de la tabla `ofertas`
 --
 ALTER TABLE `ofertas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_empresa` (`id_empresa`);
+
+--
+-- Indices de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `username_or_email_on_hold`
@@ -351,13 +421,13 @@ ALTER TABLE `denied_access`
 -- AUTO_INCREMENT de la tabla `empresas`
 --
 ALTER TABLE `empresas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `establecimientos`
 --
 ALTER TABLE `establecimientos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `ips_on_hold`
@@ -375,6 +445,12 @@ ALTER TABLE `login_errors`
 -- AUTO_INCREMENT de la tabla `ofertas`
 --
 ALTER TABLE `ofertas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `perfil`
+--
+ALTER TABLE `perfil`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -399,6 +475,25 @@ ALTER TABLE `acl`
 --
 ALTER TABLE `acl_actions`
   ADD CONSTRAINT `acl_actions_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `acl_categories` (`category_id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `establecimientos`
+--
+ALTER TABLE `establecimientos`
+  ADD CONSTRAINT `establecimientos_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id`),
+  ADD CONSTRAINT `establecimientos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`user_id`);
+
+--
+-- Filtros para la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  ADD CONSTRAINT `ofertas_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id`);
+
+--
+-- Filtros para la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD CONSTRAINT `perfil_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
